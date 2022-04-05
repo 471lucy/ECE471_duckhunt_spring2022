ad48ae38d2424e5a888ffecb972c6741a6fb9e15
471lucy <101016855+471lucy@users.noreply.github.com>
2022-04-05 16:24:56 -0700

    MATLAB scripts for testing model classification
    MATLAB scripts for testing the model on the the training datasets and the test datasets and getting percentages of correct classifications. classify_ducks.m and classify_ducks_newtest.m used for Attempt 2, while classify_ducks_64.m is used for Attempt 3.

---
+++ classify_ducks.m
@@ -0,0 +1,72 @@
+function [] = classify_ducks(ws,D_train,D_test)
+
+%training data set classification
+
+%need percent correctly identified
+
+D_train = [D_train(1:8100,:);ones(1,800)];
+size(D_train(:,1))
+size(ws)
+
+D_test = [D_test(1:8100,:);ones(1,200)];
+
+
+total_pos_train = 400;
+total_neg_train = 400;
+pos_train = 0;
+neg_train = 0;
+
+%positive duck train
+for i = 1:1:400
+    class = sign(ws'*D_train(:,i));
+    if class > 0
+        pos_train = pos_train + 1;
+    end
+end
+percent_correct_pos_train = (pos_train/total_pos_train) * 100
+
+%negative duck train
+for i = 401:1:800
+    class = sign(ws'*D_train(:,i));
+    if class < 0
+        neg_train = neg_train + 1;
+    end
+end
+percent_correct_neg_train = (neg_train/total_neg_train) * 100
+
+
+
+
+%test dataset classification
+total_pos_test = 100;
+total_neg_test = 100;
+pos_test = 0;
+neg_test = 0;
+
+%positive duck train
+for i = 1:1:100
+    class = sign(ws'*D_test(:,i));
+    if class > 0
+        pos_test = pos_test + 1;
+    end
+end
+percent_correct_pos_test = (pos_test/total_pos_test) * 100
+
+%negative duck train
+for i = 101:1:200
+    class = sign(ws'*D_test(:,i));
+    if class < 0
+        neg_test = neg_test + 1;
+    end
+end
+percent_correct_neg_test = (neg_test/total_neg_test) * 100
+
+
+
+
+
+
+
+
+
+end
